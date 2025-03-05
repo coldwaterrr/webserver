c30092e4e5cf62e00a595e5b009fd272d2df1e32
cold <807316337@qq.com>
2025-03-05 10:37:46 +0800

    简化server

--- src/impl/server.cpp
+++ src/impl/server.cpp
@@ -20,22 +20,6 @@ Server::~Server() {
     if(epoll_fd != -1) close(epoll_fd);
 }
 
-// // 将给定的文件描述符 fd 设置为非阻塞模式
-// bool Server::setNonBlocking(int fd) {
-//     // 获取当前文件描述符的状态标志
-//     int flags = fcntl(fd, F_GETFL, 0);
-//     if(flags == -1) {
-//         logger.error("fcntl F_GETFL failed");
-//         return false;
-//     }
-//     // 添加 O_NONBLOCK 标志
-//     if(fcntl(fd, F_SETFL, flags | O_NONBLOCK) == -1) {
-//         logger.error("fcntl F_SETFL failed");
-//         return false;
-//     }
-//     return true;
-// }
-
 // 建立并初始化监听 socket
 bool Server::setupSocket() {
     // 创建 TCP 套接字
@@ -45,44 +29,10 @@ bool Server::setupSocket() {
         return false;
     }
 
-    // // 设置监听套接字为非阻塞模式
-    // if(!setNonBlocking(listen_fd)) {
-    //     return false;
-    // }
-
-    // // 构造服务器地址结构体，并清零
-    // sockaddr_in server_addr;
-    // memset(&server_addr, 0, sizeof(server_addr));
-    // server_addr.sin_family = AF_INET;          // IPv4 协议族
-    // server_addr.sin_port = htons(port);          // 监听端口，转换为网络字节序
-    // server_addr.sin_addr.s_addr = INADDR_ANY;    // 绑定到任意本地 IP
-    
-    // char ip[INET_ADDRSTRLEN];
-
-    // 绑定套接字到指定地址和端口
-    // if(bind(listen_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
-    //     logger.error("bind failed");
-    //     return false;
-    // }
-
     // 绑定套接字到指定地址和端口
     socka.bind();
-
-    // socklen_t addr_len = sizeof(server_addr);
-
-    // // 获取当前的 IP 地址
-    // if (getsockname(listen_fd, (struct sockaddr*)&server_addr, &addr_len) == -1) {
-    //     logger.error("getsockname failed");
-    //     return false;
-    // }
-    // inet_ntop(AF_INET, &server_addr.sin_addr, ip, sizeof(ip));
     
-
     // 进入监听状态
-    // if(listen(listen_fd, SOMAXCONN) < 0) {
-    //     logger.error("listen failed");
-    //     return false;
-    // }
     socka.listen();
 
     logger.info("Socket setup complete on port " + std::to_string(port));
@@ -154,27 +104,13 @@ void Server::handleEvents() {
         int fd = events[i].data.fd;
         // 如果事件来自监听套接字，表示有新连接请求
         if(fd == socka.getListendFd()) {
-            // sockaddr_in client_addr;
-            // socklen_t client_len = sizeof(client_addr);
-            // 接受新连接
-            // int client_fd = accept(listen_fd, (struct sockaddr*)&client_addr, &client_len);
-            // if(client_fd < 0) {
-            //     logger.error("accept failed");
-            //     continue;
-            // }
             std::string ip = "172.17.55.172";
+            // 接受新连接
             int client_fd = socka.acceptConnection(ip);
             if(client_fd == -1) {
                 logger.error("accept failed");
                 continue;
             }
-            // logger.info("Socket successfully accept");
-
-            // // 设置新连接的 socket 为非阻塞
-            // if(!setNonBlocking(client_fd)) {
-            //     close(client_fd);
-            //     continue;
-            // }
 
             // 配置新连接的 epoll 事件，使用边缘触发模式（EPOLLET）
             epoll_event event;
